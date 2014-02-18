44cd68b1fbf2e481313f036fef967dfa61ebc538
Jenna Pederson <jennapederson@gmail.com>
2014-02-17 23:13:13 -0600

    Get back to start state

--- bowling/src/test/java/bowlorama/GameTest.java
+++ bowling/src/test/java/bowlorama/GameTest.java
@@ -1,10 +1,5 @@
 package bowlorama;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
-
-import org.junit.Before;
-import org.junit.Test;
 
 /**
  * Copyright 2014 612 Software Foundry
@@ -15,8 +10,6 @@ import org.junit.Test;
  */
 public class GameTest {
 
-	private Game game;
-
 	// one roll
 	// two rolls
 	// 4 rolls
@@ -25,57 +18,4 @@ public class GameTest {
 	// perfect game
 	// refactor
 	
-	@Before
-	public void before() {
-		game = new Game();
-	}
-	
-	@Test
-	public void testOneRoll() {
-		game.roll(5);
-
-		// verify
-		// write asserts first
-		assertThat(game.getScore(), is(5));
-		assertThat(game.getScoreForFrame(1), is(5));
-	}
-
-	@Test
-	public void testTwoRolls() {
-		// setup
-		game.roll(5);
-		game.roll(2);
-		// verify
-		assertThat(game.getScore(), is(7));
-		assertThat(game.getScoreForFrame(1), is(7));
-		
-	}
-
-	@Test
-	public void testFourRolls() {
-		game.roll(5);
-		game.roll(2); // 7
-		game.roll(1);
-		game.roll(4); // 12
-		
-		// verify
-		assertThat(game.getScore(), is(12));
-		assertThat(game.getScoreForFrame(1), is(7));
-		assertThat(game.getScoreForFrame(2), is(12));
-	}
-	
-	@Test
-	public void testSpareAndTwoRolls() {
-		// setup
-		game.roll(4);
-		game.roll(6); // spare; 12
-		game.roll(2);
-		game.roll(4); // 18
-		
-		// verify
-		assertThat(game.getScoreForFrame(1), is(12));
-		assertThat(game.getScoreForFrame(2), is(18));
-		assertThat(game.getScore(), is(18));
-	}
-
 }
