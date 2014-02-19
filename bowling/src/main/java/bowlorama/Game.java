cbbd0553e2bbb669554fa4b2ef9370c4e0af8084
Jenna Pederson <jennapederson@gmail.com>
2014-02-18 19:31:26 -0600

    Refactor

--- bowling/src/main/java/bowlorama/Game.java
+++ bowling/src/main/java/bowlorama/Game.java
@@ -2,16 +2,19 @@ package bowlorama;
 
 public class Game {
 
-	private int score = 0;
-	private int[] rolls = new int[21];
+	private static final int MAX_ROLLS_PER_GAME = 21;
+	private int[] rolls = new int[MAX_ROLLS_PER_GAME];
 	private int roll = 0;
 	private int currentFrame = 0;
 	private boolean firstRoll = true;
 
 	public void roll(int pinsKnockedDown) {
-		score += pinsKnockedDown;
 		rolls[roll++] = pinsKnockedDown;
 
+		incrementCurrentFrame();
+	}
+
+	private void incrementCurrentFrame() {
 		if (firstRoll) {
 			currentFrame++;
 			firstRoll = false;
@@ -29,10 +32,14 @@ public class Game {
 		int scoreForFrame = 0;
 
 		for (int currentFrame = 0; currentFrame < frame; currentFrame++) {
-			scoreForFrame += rolls[ball]; // roll 1
-			scoreForFrame += rolls[ball + 1]; // roll 2
-			if ((rolls[ball] + rolls[ball + 1]) == 10) {
-				scoreForFrame += rolls[ball + 2]; // roll 3
+			int roll1 = rolls[ball];
+			int roll2 = rolls[ball + 1];
+			int roll3 = rolls[ball + 2];
+
+			scoreForFrame += roll1; // roll 1
+			scoreForFrame += roll2; // roll 2
+			if ((roll1 + roll2) == 10) {
+				scoreForFrame += roll3; // roll 3
 			}
 			ball += 2;
 		}
